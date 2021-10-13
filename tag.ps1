e4305f7ee000b79cea99cd2fb61338ab62c92f96
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-10-13 13:29:42 +0300

    test

--- tag.ps1
+++ tag.ps1
@@ -5,12 +5,23 @@ Get-Content full_tag_list.json `
 | ForEach-Object {$_ -split "/"} `
 | Select-String -Pattern "^v0.0" `
 | ForEach-Object {$_ -match "v\d.\d.(?<version>.+)" | Out-Null; $versions += $Matches.version} 
-$versions | Where-Object -FilterScript { $_ -gt 1 }
 
+$latest = ($versions `
+| Where-Object -FilterScript { $_ -gt 99 } `
+| Where-Object -FilterScript { $_ -lt 200 } `
+| Measure-Object).Maximum
 
+if($latest -eq 199) {
+  Write-Output "No new minor versions available"
+  exit 1;
+}
+elseif($latest -eq $NULL) {
+  $latest = 100
+}
 
-
-
+$latest++
+$tag = "v0.0" + "." + $latest.ToString()
+echo $tag
 
 
 
